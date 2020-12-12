1f0733d705d1bc0f2d2e77770abed55a9a3eea6f
Mohammad3125 <farsroid312@gmail.com>
2020-12-12 10:06:47 +0330

    add fragment for setting layout

--- app/src/main/java/com/example/bluetoothtest/view/fragments/FragmentSettingPreferences.java
+++ app/src/main/java/com/example/bluetoothtest/view/fragments/FragmentSettingPreferences.java
@@ -1,22 +1,26 @@
 package com.example.bluetoothtest.view.fragments;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.bluetoothtest.BuildConfig;
-import com.example.bluetoothtest.view.activities.LoginActivity;
 import com.example.bluetoothtest.R;
-import com.example.bluetoothtest.view.activities.MainActivity;
+import com.example.bluetoothtest.utility.WindowSetting;
 import com.example.bluetoothtest.view.activities.SplashScreen;
 
-public class FragmentSettingPreferences extends PreferenceFragmentCompat {
+/*public class FragmentSettingPreferences extends PreferenceFragmentCompat {
 
     Preference signOutPreference;
     Preference appVersionPreference;
@@ -53,4 +57,79 @@ public class FragmentSettingPreferences extends PreferenceFragmentCompat {
     }
 
 
+}*/
+
+public class FragmentSettingPreferences extends Fragment implements View.OnClickListener {
+    private LinearLayout logoutLayout;
+    private LinearLayout usersLayout;
+    private LinearLayout profileLayout;
+    private LinearLayout themeLayout;
+    private LinearLayout startupTimeLayout;
+    private LinearLayout permissionLayout;
+    private TextView versionTextView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        initViews(view);
+
+        WindowSetting windowSetting = new WindowSetting(requireActivity().getWindow());
+
+        windowSetting.windowsFullScreen().setStatusBarColor(ContextCompat.getColor(getContext(), R.color.colorBackgroundDarker));
+
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        versionTextView.setText(BuildConfig.VERSION_NAME); //setting app version into text view in setting fragment
+
+        logoutLayout.setOnClickListener(this);
+        usersLayout.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    private void initViews(View view) {
+        versionTextView = view.findViewById(R.id.setting_page_text_view_app_version);
+        logoutLayout = view.findViewById(R.id.setting_page_layout_logout);
+        profileLayout = view.findViewById(R.id.setting_page_layout_profile);
+        permissionLayout = view.findViewById(R.id.setting_page_layout_permission);
+        startupTimeLayout = view.findViewById(R.id.setting_page_layout_startup_time);
+        usersLayout = view.findViewById(R.id.setting_page_layout_users);
+        themeLayout = view.findViewById(R.id.setting_page_layout_theme);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == logoutLayout.getId()) {
+            getContext().
+                    getSharedPreferences(SplashScreen.SHARED_PREFERENCES_TAG, Context.MODE_PRIVATE).
+                    edit().putBoolean(SplashScreen.LOGIN_STATE_KEY, false).apply();
+
+            Navigation.findNavController(view)
+                    .navigate(FragmentSettingPreferencesDirections.
+                            actionFSettingPreferencesToLoginHost());
+
+        }
+
+        if (view.getId() == usersLayout.getId())
+            Navigation.findNavController(view).
+                    navigate(FragmentSettingPreferencesDirections.
+                            actionFSettingPreferencesToFUserAdd());
+
+
+    }
 }
